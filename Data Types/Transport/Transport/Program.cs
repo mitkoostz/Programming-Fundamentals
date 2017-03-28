9ae65c35edb3c65cab73e31e2cfbff21bf393346
mitkoostz <todorinio123@gmail.com>
2017-03-28 21:18:22 +0300

    Transport lab

---
+++ Data Types/Transport/Transport/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int courses = (int)Math.Ceiling((double)n / 24);
+
+            Console.WriteLine(courses);
+
+
+        }
+    }
+}
