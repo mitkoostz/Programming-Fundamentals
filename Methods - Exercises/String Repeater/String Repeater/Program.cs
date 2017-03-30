642d02575a9e644b17b6f40069c55fb95aa402b8
mitkoostz <todorinio123@gmail.com>
2017-03-30 23:51:03 +0300

    Methods - Exercises

---
+++ Methods - Exercises/String Repeater/String Repeater/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Repeater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int count = int.Parse(Console.ReadLine());
+          
+            Console.WriteLine(StringRepeat(text, count));
+        }
+
+        private static string StringRepeat(string str,int count)
+        {
+            string s = "";
+            for (int i = 0; i < count; i++)
+            {
+                s = s + str;
+            }
+            return s;
+        }
+    }
+}
