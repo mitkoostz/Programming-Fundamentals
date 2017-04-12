dd78dd5f67f3c19a2a796d17eadf572962a361ed
mitkoostz <todorinio123@gmail.com>
2017-04-12 21:46:34 +0300

    List - Exercises

---
+++ Lists - Exercises/RemoveElementAtOddPosition/RemoveElementAtOddPosition/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveElementAtOddPosition
+{
+    class RemoveElementAtOddPosition
+    {
+        public static void Main()
+        {
+
+            var list = Console.ReadLine().Split(' ').ToList();
+
+            var indexToSave = 1;
+
+            var inputedListCount = list.Count;
+
+            var result = new List<string>();
+
+            for (int i = 0; i < inputedListCount; i++)
+            {
+
+                if (i == indexToSave)
+                {
+                    result.Add(list[i]);
+                    indexToSave += 2;
+                }
+
+                
+            }
+
+            Console.WriteLine(string.Join("",result));
+
+        }
+    }
+}
